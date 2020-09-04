486c1adceb8ff28ff7f7883fa0faf105b6cb893e
Avinash Gala <agala@usc.edu>
2020-09-04 00:59:06 -0700

    trying to ifx refresh issues part 6

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -48,9 +48,9 @@ if __name__ == '__main__':
         ultrastring = str(ultrasonic_value)
 
         if ultrasonic_value <= sensor_value:
-            setText(sensorstring + "  " + "OBJ PRES" + "\n" + ultrastring)
+            setText_norefresh(sensorstring + "  " + "OBJ PRES" + "\n" + ultrastring)
         else:
-            setText(sensorstring + "\n" + ultrastring)
+            setText_norefresh(sensorstring + "          " + "\n" + ultrastring)
         #print ("sensor_value = %d" % sensor_value)
         
         time.sleep(0.2)
